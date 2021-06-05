6d31a4ef1f232998cb3bba6100287c375e16df89
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-06-05 05:06:59 +0300

    second

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,13 +7,11 @@ const Footer = (props) => {
   const { doneCount, clearCompleted, filter, onFilterChange } = props;
 
   Footer.defaultProps = {
-    // дефолтные пропсы
     clearCompleted: () => {},
     onFilterChange: () => {},
   };
 
   Footer.propTypes = {
-    // проверяем является ли doneCount числом, но чет ему совсем пох)
     clearCompleted: PropTypes.func,
     onFilterChange: PropTypes.func,
     doneCount: PropTypes.number.isRequired,
@@ -22,7 +20,10 @@ const Footer = (props) => {
 
   return (
     <footer className="footer">
-      <span className="todo-count">{doneCount} items left</span>
+      <span className="todo-count">
+        {doneCount}
+        items left
+      </span>
       <TaskFilter filter={filter} onFilterChange={onFilterChange} />
       <button type="button" className="clear-completed" onClick={clearCompleted}>
         Clear completed
