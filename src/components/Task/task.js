606e182c36b56662456e4e67ede7154b701a33fd
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-07-15 15:26:16 +0300

    completed

--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -1,125 +1,94 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './task.css';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
-export default class Task extends Component {
-  state = {
-    pastTime: null,
-    timeSec: null,
-    id: null,
-    playPause: false,
+function Task({ label, classNameLi, done, onDeleted, onNotDone, сreationTime, timeSec, id, pastTime, setTime }) {
+  const [pastTimeSet, setPastTime] = useState(pastTime);
+  const [timeSecSet, setTimeSec] = useState(timeSec);
+  const [playPauseSet, setPlayPause] = useState(false);
+
+  const playTime = () => {
+    setPlayPause(true);
   };
 
-  static defaultProps = {
-    onDeleted: () => {},
-    onNotDone: () => {},
-    setTime: () => {},
+  const pauseTime = () => {
+    setPlayPause(false);
   };
 
-  static propTypes = {
-    onDeleted: PropTypes.func,
-    onNotDone: PropTypes.func,
-    setTime: PropTypes.func,
-    label: PropTypes.string.isRequired,
-    done: PropTypes.bool.isRequired,
-    classNameLi: PropTypes.string.isRequired,
-    сreationTime: PropTypes.objectOf(PropTypes.object).isRequired,
-    id: PropTypes.string.isRequired,
-    timeSec: PropTypes.number.isRequired,
-    pastTime: PropTypes.string.isRequired,
-  };
+  useEffect(() => {
+    let timeCreationId = null;
+    let timerId = null;
 
-  componentDidMount() {
-    /* монтирование */
-    const { timeSec, id, pastTime } = this.props;
+    timeCreationId = setInterval(() => {
+      const newPastTime = ` ${formatDistanceToNow(сreationTime, { addSuffix: true })}`;
+      setPastTime(newPastTime);
+    }, 300000);
 
-    this.setState({
-      timeSec,
-      id,
-      pastTime,
-    });
-
-    this.intervalID = setInterval(this.pastTimeTask, 300000);
-  }
-
-  componentDidUpdate() {
-    /* обновление состояния */
-    const { playPause } = this.state;
-
-    if (playPause === true) {
-      clearInterval(this.timerId);
-      this.timerId = setInterval(this.tick, 1000);
+    if (playPauseSet) {
+      timerId = setInterval(() => {
+        setTimeSec(timeSecSet - 1);
+      }, 1000);
     }
 
-    if (playPause === false) {
-      clearInterval(this.timerId);
+    if (!playPauseSet || timeSecSet === 0 || done) {
+      clearInterval(timerId);
     }
-  }
 
-  componentWillUnmount() {
-    /* размонтирование */
-    const { setTime } = this.props;
-    const { timeSec, id, pastTime } = this.state;
+    return () => {
+      clearInterval(timerId);
+      clearInterval(timeCreationId);
+    };
+  }, [playPauseSet, timeSecSet, done, сreationTime]);
 
-    clearInterval(this.timerId);
-    clearInterval(this.intervalID);
-    setTime(timeSec, id, pastTime);
-  }
+  useEffect(() => () => setTime(timeSecSet, id, pastTimeSet));
 
-  tick = () => {
-    const { timeSec } = this.state;
-    const { done } = this.props;
+  const hours = Math.floor(timeSecSet / 60);
+  const minutes = timeSecSet % 60 < 10 ? `0${timeSecSet % 60}` : timeSecSet % 60;
+  const timeExecutionTask = `${hours}:${minutes}`;
 
-    if (timeSec === 0 || done === true) {
-      this.pauseTime();
-    } else this.setState({ timeSec: timeSec - 1 });
-  };
-
-  playTime = () => {
-    this.setState({ playPause: true });
-  };
-
-  pauseTime = () => {
-    this.setState({ playPause: false });
-  };
-
-  pastTimeTask = () => {
-    const { сreationTime } = this.props;
-    this.setState({ pastTime: ` ${formatDistanceToNow(сreationTime, { addSuffix: true })}` });
-  };
-
-  render() {
-    const { label, classNameLi, done, onDeleted, onNotDone } = this.props;
-    const { pastTime, timeSec } = this.state;
-
-    const hours = Math.floor(timeSec / 60);
-    const minutes = timeSec % 60 < 10 ? `0${timeSec % 60}` : timeSec % 60;
-    const timeExecutionTask = `${hours}:${minutes}`;
-
-    return (
-      <li className={classNameLi}>
-        <div className="view">
-          <input className="toggle" type="checkbox" readOnly="readonly" checked={done} onClick={onNotDone} />
-          <div className="label">
-            <span aria-hidden="true" className="description" onClick={onNotDone}>
-              {label}
-            </span>
-            <span className="timer">
-              <button type="button" onClick={this.playTime} className="icon icon-play" aria-label="play" />
-              <button type="button" onClick={this.pauseTime} className="icon icon-pause" aria-label="pause" />
-              {timeExecutionTask}
-            </span>
-            <span className="created">
-              created
-              {pastTime}
-            </span>
-
-            <button aria-label="edit" type="button" className="icon icon-edit" />
-            <button aria-label="destroy" type="button" className="icon icon-destroy" onClick={onDeleted} />
-          </div>
+  return (
+    <li className={classNameLi}>
+      <div className="view">
+        <input className="toggle" type="checkbox" readOnly="readonly" checked={done} onClick={onNotDone} />
+        <div className="label">
+          <span aria-hidden="true" className="description" onClick={onNotDone}>
+            {label}
+          </span>
+          <span className="timer">
+            <button type="button" onClick={playTime} className="icon icon-play" aria-label="play" />
+            <button type="button" onClick={pauseTime} className="icon icon-pause" aria-label="pause" />
+            {timeExecutionTask}
+          </span>
+          <span className="created">
+            created
+            {pastTimeSet}
+          </span>
+          <button aria-label="edit" type="button" className="icon icon-edit" />
+          <button aria-label="destroy" type="button" className="icon icon-destroy" onClick={onDeleted} />
         </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
 }
+
+Task.defaultProps = {
+  onDeleted: () => {},
+  onNotDone: () => {},
+  setTime: () => {},
+};
+
+Task.propTypes = {
+  onDeleted: PropTypes.func,
+  onNotDone: PropTypes.func,
+  setTime: PropTypes.func,
+  label: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+  classNameLi: PropTypes.string.isRequired,
+  сreationTime: PropTypes.objectOf(PropTypes.object).isRequired,
+  id: PropTypes.string.isRequired,
+  timeSec: PropTypes.number.isRequired,
+  pastTime: PropTypes.string.isRequired,
+};
+
+export default Task;
